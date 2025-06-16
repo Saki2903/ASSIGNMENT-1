1335813e5be828feb279f1931e1307275f1202ff
Saki2903 <own.sakirul@gmail.com>
2025-06-16 17:18:31 +0530

    Update and rename FirstProgram.py to Task1.py

--- FirstProgram.py
+++
@@ -1,4 +0,0 @@
-frist_name=input("Enter your First Name: ")
-last_name=input("Enter your Last Name: ")
-name=frist_name+" "+last_name
-print("Hello,",name,"Welcome to the python program.")
---
+++ Task1.py
@@ -0,0 +1,4 @@
+first_name = input("Enter your First Name: ")
+last_name = input("Enter your Last Name: ")
+name = first_name + " " + last_name
+print("Hello, " + name + "! Welcome to the python program.")
